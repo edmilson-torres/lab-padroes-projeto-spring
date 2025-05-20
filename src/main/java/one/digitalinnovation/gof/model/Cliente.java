81058ecd0b90307c1e8df3f7c947a426a659cfa8
Edmilson Torres <edmilson.torres@gmail.com>
2025-05-20 20:57:01 -0300

    refactor: update Cliente and Endereco models with validation annotations and remove Lombok annotations

--- src/main/java/one/digitalinnovation/gof/model/Cliente.java
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -5,14 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 @Entity
 public class Cliente {
 
@@ -20,11 +16,35 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@NotNull
-	@Size(min = 2, max = 30)
+	@NotBlank(message = "O nome do cliente é obrigatório")
+	@Size(min = 2, max = 30, message = "O nome deve ter entre 2 e 30 caracteres")
 	private String nome;
 
 	@ManyToOne
+	@Valid
 	private Endereco endereco;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
 }
